195e3ff3ca207d449568b1b445afb7131d0284b4
Rickjon <121823202+Rickjon@users.noreply.github.com>
2023-01-03 01:42:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('WHY THE HELL ARE YOU STILL STUCK?');
+let name = 'Mosh'; //String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;
